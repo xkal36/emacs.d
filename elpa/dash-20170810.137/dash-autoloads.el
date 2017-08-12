9a99c39e18b80af755f418de9eee3abb03ea50d8
Andrew Winterbotham <awinterbotham02@gmail.com>
2017-08-12 21:05:56 +0000

    add gitignore

--- elpa/dash-20170810.137/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (22927 27356 574919 444000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
